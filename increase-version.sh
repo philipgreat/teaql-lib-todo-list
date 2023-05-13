ea24eaca3d6d78594dc08fc8523c89775a787e5f
Philip Zhang <zhangxilai@doublechaintech.com>
2023-05-13 12:33:44 +0800

    update to version: 124

--- increase-version.sh
+++ increase-version.sh
@@ -1,6 +1,4 @@
-
-version=$(bash get-version.sh)
-
-content=$(xmlstarlet ed -u '/root/@version' -v ${version} models/main.xml > temp.xml)
-
+new_version=$(bash get-version.sh)
+xmlstarlet ed -u '/root/@version' -v ${new_version} models/main.xml > temp.xml
 cat temp.xml > models/main.xml
+rm temp.xml
