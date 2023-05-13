cd9cc2ff778876c08f5230fb88bbae38cb002538
Philip Zhang <zhangxilai@doublechaintech.com>
2023-05-13 12:20:59 +0800

    update to version: 107

--- increase-version.sh
+++ increase-version.sh
@@ -1,12 +1,2 @@
-xml_content=$(cat models/main.xml)
 
-# Get the value of the version attribute
-version=$(xmllint  --xpath "string(/root/@version)"  models/main.xml)
-# Increase the version by 1
-new_version=$((version + 1))
-
-# Print the updated XML content
-echo "$new_version"
-
-
-cat models/main.xml | xmllint --shell - | sed 's/version="\(.*\)"/set_attr("version", \1 + 1)/' | xmllint --format -
+xmlstarlet ed -u '//version/@number' -v 108 models/main.xml
